08a60d914957bfd37a2f2ee43e82e5bacd3321bc
Aidan Dannhausen-Brun <adannhausenbrun@gmail.com>
2025-07-22 20:55:50 -0500

    added asteroid sprite and its byte array

--- Core/Inc/sprites.h
+++ Core/Inc/sprites.h
@@ -13,7 +13,6 @@ extern uint8_t spaceship_sprite[];
 
 #define NUMBER_HEIGHT 20
 #define NUMBER_WIDTH 14
-
 extern uint8_t number_0[];
 extern uint8_t number_1[];
 extern uint8_t number_2[];
@@ -24,4 +23,10 @@ extern uint8_t number_6[];
 extern uint8_t number_7[];
 extern uint8_t number_8[];
 extern uint8_t number_9[];
+
+
+#define ASTEROID_SIZE 40
+extern uint8_t asteroid[];
+
+
 #endif
